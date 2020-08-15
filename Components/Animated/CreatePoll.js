f0b7df32ed77c9493c482622d16418bb169a3300
TegVeer <tegu576@gmail.com>
2020-08-15 11:59:52 +0530

    WIP DONE 15 AUG 2020 - (Creating Polling Component for Post UI)

--- Components/Animated/CreatePoll.js
+++ Components/Animated/CreatePoll.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import Colors from "../../Constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import { Col } from "native-base";
 
 export default function CreatePoll() {
-  const [count, setCount] = useState(0);
   const [optionsNumber, setoptionsNumber] = useState([
     { id: 1, text: "" },
     { id: 2, text: "" },
   ]);
 
-  function Options() {
+  function Options(props) {
+    const [index, setIndex] = useState(props.index);
+    const [charCount, setCharCount] = useState(0);
+
     return (
       <View style={{ marginTop: 5, flexDirection: "row" }}>
         <View
@@ -31,11 +33,20 @@ export default function CreatePoll() {
                 paddingVertical: 5,
                 paddingHorizontal: 10,
               }}
-              placeholder="Option 1"
+              placeholder={
+                index > 1
+                  ? `Option ${index + 1} (Optional)`
+                  : `Option ${index + 1}`
+              }
+              onChangeText={(text) => {
+                setCharCount(text.length);
+                optionsNumber[index].text = text;
+              }}
+              value={optionsNumber[index].text}
             />
           </View>
           <Text style={{ color: Colors.greyText, fontSize: 12 }}>
-            {count}/50
+            {charCount}/50
           </Text>
         </View>
         <View style={{ marginHorizontal: 10, padding: 10 }}></View>
@@ -47,7 +58,6 @@ export default function CreatePoll() {
     <View style={{ flex: 1, justifyContent: "center", alignItem: "center" }}>
       <View
         style={{
-          minHeight: 200,
           marginHorizontal: 10,
           alignSelf: "stretch",
           borderRadius: 20,
@@ -71,9 +81,41 @@ export default function CreatePoll() {
           </View>
           <Ionicons name="ios-close" size={24} color="black" />
         </View>
-        {optionsNumber.map((item) => (
-          <Options key={item.id} />
+        {optionsNumber.map((item, index) => (
+          <Options key={item.id} index={index} />
         ))}
+        <View
+          style={{
+            flexDirection: "row",
+            alignSelf: "stretch",
+            backgroundColor: Colors.light,
+            alignItems: "center",
+          }}
+        >
+          <View style={{ paddingHorizontal: 10, paddingVertical: 5 }}>
+            <Text>Duration</Text>
+          </View>
+          <View style={{ flex: 1, paddingHorizontal: 10, paddingVertical: 5 }}>
+            <Text style={{ color: Colors.likedBlue }}>1 Day</Text>
+          </View>
+          <TouchableOpacity
+            onPress={() => {
+              if (optionsNumber.length < 5) {
+                setoptionsNumber((initialState) => {
+                  return [
+                    ...initialState,
+                    { id: optionsNumber.length + 1, text: "" },
+                  ];
+                });
+              } else {
+                console.log("Cant add more than 5 options");
+              }
+            }}
+            style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+          >
+            <Ionicons name="ios-add" size={24} color={Colors.black} />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
